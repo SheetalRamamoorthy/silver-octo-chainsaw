8dec3e4653d56e154687fbd089edada90e4b59aa
SheetalRamamoorthy <42152457+SheetalRamamoorthy@users.noreply.github.com>
2019-04-02 10:31:33 -0400

    Add files via upload

---
+++ Homicide.R
@@ -0,0 +1,6 @@
+Homicide_dataset=read.csv(file = "database.csv",header = TRUE,sep = ",")
+Homicide_dataset[1:10,]
+nrow(Homicide_dataset)
+ncol(Homicide_dataset)
+table(is.na(Homicide_dataset))
+638454*24
